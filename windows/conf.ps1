ba3652d045f916350765b646d15ef42e4c73eeae
Robert F Sonders <rfsonders@gmail.com>
2023-01-31 17:26:04 -0700

    Cleaned up for readability
    Clean up config to showcase in paper.

--- windows/conf.ps1
+++ windows/conf.ps1
@@ -1,15 +1,42 @@
-#Kubernetes related configs
+
+## --------------------------------------------------
+# Kubernetes related configs
+## --------------------------------------------------
 $hammerdbNamespace = 'hammer-db-ns'
+
+## --------------------------------------------------
 # SQL Server related configs
-$sqlEnv="linux"  # this variable can be set to either of these two values (linux or windows)
+## --------------------------------------------------
+# Can be set to either of these two values (linux or windows)
+$sqlEnv="linux"  
+
+# IP addresses of each SQL MI. Add prts if NodePort K8s configuratin
 $mssqlips="10.129.80.55", "10.129.80.56", "10.129.80.57", "10.129.80.58", "10.129.80.59" 
+
+# MI username
 $mssqlUser='miadmin'
+
+# MI Password
 $mssqlPass='!!123abc' 
+
+# TPC database
 $mssqlDatabase='tpcc'
-$backupLocation="/var/opt/mssql/backups"  #Location to keep backup and other useful scripts (do not provide back slash (/) at the end of the path)
+
+# SQL backup file location
+# (do not provide back slash (/) at the end of the path)
+$backupLocation="/var/opt/mssql/backups"  
+
+## --------------------------------------------------
 # Load run config
-# rampupTime and execTime in minutes per user load
-$userLoadSet=3
-$loadRunUser='10 20 30'
+## --------------------------------------------------
+# RampupTime and execTime in minutes per user load
+$userLoadSet=5
+
+# User load test groups
+$loadRunUser='10 20 30 40 50'
+
+# HammerDb Ramp up time
 $rampupTime=1 
+
+# Execute time per user group
 $execTime=5
